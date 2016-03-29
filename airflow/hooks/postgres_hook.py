026dabec5acb418bda04452dc88fcc5039e53910
Bence Nagy <bence@underyx.me>
2016-03-29 13:33:42 +0200

    Set Postgres autocommit as supported only if server version is < 7.4
    The server-side autocommit setting was removed here http://www.postgresql.org/docs/7.4/static/release-7-4.html
    
    Resolves: #690

--- airflow/hooks/postgres_hook.py
+++ airflow/hooks/postgres_hook.py
@@ -9,7 +9,7 @@ class PostgresHook(DbApiHook):
     '''
     conn_name_attr = 'postgres_conn_id'
     default_conn_name = 'postgres_default'
-    supports_autocommit = True
+    supports_autocommit = False
 
     def get_conn(self):
         conn = self.get_connection(self.postgres_conn_id)
@@ -19,3 +19,12 @@ class PostgresHook(DbApiHook):
             password=conn.password,
             dbname=conn.schema,
             port=conn.port)
+
+        # check for ssl parameters in conn.extra
+        for arg_name, arg_val in conn.extra_dejson.items():
+            if arg_name in ['sslmode', 'sslcert', 'sslkey', 'sslrootcert', 'sslcrl']:
+                conn_args[arg_name] = arg_val
+        psycopg2_conn = psycopg2.connect(**conn_args)
+        if psycopg2_conn.server_version < 70400:
+            self.supports_autocommit = True
+        return psycopg2_conn
